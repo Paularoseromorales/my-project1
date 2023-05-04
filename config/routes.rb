415e4469829494be37711786c832f8b7e1d5085f
paularosero <pao-0114@hotmail.com>
2023-05-04 12:45:13 -0400

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   # verb "path", to: "controller#action"
-  root to: 'pages#home'
-  get 'about', to: 'pages#about', as: :about
-  get 'contact', to: 'pages#contact', as: :contact
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+  patch 'restaurants/:id', to: 'restaurants#update'
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get 'restaurants', to: 'restaurants#index'
+  resources :restaurant
 end
