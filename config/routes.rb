b68ea83684f6f69b3df0a63d3ef63a9da42db69e
paularosero <pao-0114@hotmail.com>
2023-05-03 22:06:07 -0400

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # verb "path", to: "controller#action"
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
 end
